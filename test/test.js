12105c5990b72a0c29d618b2537a1b42083decb1
cenfun <cenfun@qq.com>
2022-10-28 14:52:38 +0800

    update deps

--- test/test.js
+++ test/test.js
@@ -1,27 +1,27 @@
-const fs = require("fs");
-const path = require("path");
-const UPNG = require("upng-js");
-const ScreencastGIF = require("screencast-gif");
+const fs = require('fs');
+const path = require('path');
+const UPNG = require('upng-js');
+const ScreencastGIF = require('screencast-gif');
 
-const WasmGifEncoder = require("../lib");
+const WasmGifEncoder = require('../lib');
 console.log(WasmGifEncoder);
-console.log("number add: " + WasmGifEncoder.add(123, 456));
-console.log("string join: " + WasmGifEncoder.str("str1 ", "str2"));
-console.log("==========================================================");
+console.log(`number add: ${WasmGifEncoder.add(123, 456)}`);
+console.log(`string join: ${WasmGifEncoder.str('str1 ', 'str2')}`);
+console.log('==========================================================');
 
 const mergeColor = function(c, b, p) {
-    let v = c * p + b * (1 - p);
+    const v = c * p + b * (1 - p);
     return Math.round(v);
 };
 
 const getImagePixels = (image, w, h, bgColor = 0xffffff) => {
 
-    let bgR = bgColor >> 16 & 0xff;
-    let bgG = bgColor >> 8 & 0xff;
-    let bgB = bgColor & 0xff;
+    const bgR = bgColor >> 16 & 0xff;
+    const bgG = bgColor >> 8 & 0xff;
+    const bgB = bgColor & 0xff;
 
-    let l = w * h;
-    //png rgba 4 => rgb 3
+    const l = w * h;
+    // png rgba 4 => rgb 3
     const pixels = new Uint8Array(l * 3);
     let i = 0;
     let j = 0;
@@ -30,17 +30,17 @@ const getImagePixels = (image, w, h, bgColor = 0xffffff) => {
         let r = image[p++];
         let g = image[p++];
         let b = image[p++];
-        let a = image[p++];
+        const a = image[p++];
         if (a === 0) {
             r = bgR;
             g = bgG;
             b = bgB;
         } else if (a !== 255) {
             // console.log(r, g, b, a);
-            let p = a / 255;
-            r = mergeColor(r, bgR, p);
-            g = mergeColor(g, bgG, p);
-            b = mergeColor(b, bgB, p);
+            const per = a / 255;
+            r = mergeColor(r, bgR, per);
+            g = mergeColor(g, bgG, per);
+            b = mergeColor(b, bgB, per);
             // console.log(r, g, b);
         }
         pixels[j++] = r;
@@ -55,62 +55,68 @@ const getImagePixels = (image, w, h, bgColor = 0xffffff) => {
 const generateGif = (name) => {
 
     let time_start;
-    let folder = "test/example/" + name;
-    let gifpath = folder + ".gif";
-    let frames = fs.readdirSync(folder).map((pngname) => {
-        return folder + "/" + pngname;
+    const folder = path.resolve('test/example/');
+
+    const pngFolder = path.resolve(folder, name);
+    const frames = fs.readdirSync(pngFolder).map((pngName) => {
+        return path.resolve(pngFolder, pngName);
     });
 
-    //frames.length = 2;
+    if (!frames.length) {
+        throw new Error(`Not found PNGs: ${pngFolder}`);
+    }
 
-    console.log("start to generate: " + gifpath + " (" + frames.length + " frames) ...");
+    // frames.length = 2;
 
-    let fileBufs = frames.map(function(p) {
-        return fs.readFileSync(path.resolve(p));
+    const gifPath = path.resolve(folder, `${name}.gif`);
+    console.log(`start to generate: ${gifPath} (${frames.length} frames) ...`);
+
+    const fileBuffers = frames.map(function(p) {
+        return fs.readFileSync(p);
     });
 
-    //node ================================================================
+    // node ================================================================
     time_start = Date.now();
-    let bufN = ScreencastGIF({
+    const bufN = ScreencastGIF({
         frame: {
             delay: 500
         },
-        frames: fileBufs
+        frames: fileBuffers
     });
-    fs.writeFileSync(folder + "-node.gif", bufN);
-    console.log("node cost: " + (Date.now() - time_start).toLocaleString() + "ms");
+    fs.writeFileSync(path.resolve(folder, `${name}-node.gif`), bufN);
+    console.log(`node cost: ${(Date.now() - time_start).toLocaleString()}ms`);
 
-    //wasm ================================================================
+    // wasm ================================================================
     time_start = Date.now();
-    let frameList = fileBufs.map(fileBuf => {
-        let img = UPNG.decode(fileBuf);
-        let data = getImagePixels(img.data, img.width, img.height);
+    const frameList = fileBuffers.map((fileBuf) => {
+        const img = UPNG.decode(fileBuf);
+        const data = getImagePixels(img.data, img.width, img.height);
         return {
             width: img.width,
             height: img.height,
             data: data
         };
     });
-    let maxWidth = Math.max.apply(Math, frameList.map(frame => frame.width));
-    let maxHeight = Math.max.apply(Math, frameList.map(frame => frame.height));
-    let info_list = [];
+    const maxWidth = Math.max.apply(Math, frameList.map((frame) => frame.width));
+    const maxHeight = Math.max.apply(Math, frameList.map((frame) => frame.height));
+    const info_list = [];
     let totalLength = 0;
-    frameList.forEach(frame => {
-        let len = frame.data.length;
+    frameList.forEach((frame) => {
+        const len = frame.data.length;
         totalLength += len;
         info_list.push(len);
         info_list.push(frame.width);
         info_list.push(frame.height);
     });
-    //console.log("totalLength: " + totalLength);
-    let data_list = Buffer.concat(frameList.map(frame => frame.data), totalLength);
-    let bufW = WasmGifEncoder.encode_gif(maxWidth, maxHeight, frameList.length, info_list, data_list);
-    fs.writeFileSync(folder + "-wasm.gif", bufW);
-    console.log("wasm cost: " + (Date.now() - time_start).toLocaleString() + "ms");
+    // console.log("totalLength: " + totalLength);
+    const data_list = Buffer.concat(frameList.map((frame) => frame.data), totalLength);
+    const bufW = WasmGifEncoder.encode_gif(maxWidth, maxHeight, frameList.length, info_list, data_list);
+    fs.writeFileSync(path.resolve(folder, `${name}-wasm.gif`), bufW);
+    console.log(`wasm cost: ${(Date.now() - time_start).toLocaleString()}ms`);
 
 };
 
-generateGif("screenshot");
-generateGif("elf");
-generateGif("cat");
-generateGif("photo");
+generateGif('screenshot');
+// generateGif('elf');
+// generateGif('cat');
+// generateGif('photo');
